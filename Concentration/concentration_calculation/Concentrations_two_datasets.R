5478b1a3729cf8b18eff2815b1740f17d9fbe389
Magda Guadalupe Cardozo Mino <mcardozo@aphros.awi.de>
2019-04-10 17:15:55 +0200

    Updated script. Includes analyses with relative abundances of Fish and DAPI counts for PCA and RDA plots. Detected error in “rda_plot” where function “evals_prop” is not giving the correct value, for now the “Proportion explained” value has to be added manually. A NMDS plot  in Images directory for surface fish counts and first test with Anosim and Permanova for significance differences between “ice-covered” regions and “ice free” was also included in the script.

--- Concentration/concentration_calculation/Concentrations_two_datasets.R
+++ Concentration/concentration_calculation/Concentrations_two_datasets.R
@@ -23,7 +23,7 @@ theme_plot <- theme(axis.title.x = element_text(size = 18),
                     panel.grid.major = element_line(linetype = 'dotted', color = 'grey', size= 0.5))
 
 ###################################
-
+# WARNING: make sure to clean the environment, silence and re-run whatever is necesary for the statistical analyses
 ###################################
 ## import data set
 ###################################
@@ -86,10 +86,10 @@ counts.aggregated_DAPI$Region[counts.aggregated_DAPI$StationName %in% EGC] <- "E
 counts.aggregated_DAPI$Region[counts.aggregated_DAPI$StationName %in% HG] <- "HG"
 counts.aggregated_DAPI$Region[counts.aggregated_DAPI$StationName %in% N] <- "N"
 counts.aggregated_DAPI$Region[counts.aggregated_DAPI$StationName %in% SV] <- "SV"
-#counts.aggregated_DAPI$Region[counts.aggregated_DAPI$StationName %in% WSC] <- "WSC"
+counts.aggregated_DAPI$Region[counts.aggregated_DAPI$StationName %in% WSC] <- "WSC"
 
 counts.aggregated_FISH$Region[counts.aggregated_FISH$StationName %in% EGC] <- "EGC"
-#counts.aggregated_FISH$Region[counts.aggregated_FISH$StationName %in% WSC] <- "WSC" # uncomment if necessary
+counts.aggregated_FISH$Region[counts.aggregated_FISH$StationName %in% WSC] <- "WSC" # uncomment if necessary
 counts.aggregated_FISH$Region[counts.aggregated_FISH$StationName %in% HG] <- "HG"
 counts.aggregated_FISH$Region[counts.aggregated_FISH$StationName %in% N] <- "N"
 counts.aggregated_FISH$Region[counts.aggregated_FISH$StationName %in% SV] <- "SV"
@@ -173,7 +173,7 @@ Plot_east_west_EUB.p <- ggplot(na.omit(subset(data.eub, Depth %in% c("DCM", "MES
 
 ##Data standarization. By default, this function will standardize the data (mean zero, unit variance). To indicate that we just want to subtract the mean, we need to turn off the argument scale = FALSE.
 env_raw <- metadata
-env_raw <- subset(env_raw, StationName!="HG1") #remove HG1 (NA)
+#env_raw <- subset(env_raw, StationName!="HG1") #remove HG1 (NA) # silence to run adonis and permanova analyses
 
 centre_scale2 <- function(x) {
    scale(x, scale = FALSE)
@@ -244,7 +244,7 @@ res3.2 # rho -0.4157426
 
 surface <- c("DCM", "EPI")
 env.surf <- subset(env[env$Depth %in% surface,])
-env.surf <- subset(env.surf, StationName!="HG1")
+#env.surf <- subset(env.surf, StationName!="HG1") # silence to run RDA and PCA
 env.surf$Region[env.surf$StationName %in% EGC] <- "EGC"
 env.surf$Region[env.surf$StationName %in% WSC] <- "WSC"
 #env.surf$Region[env.surf$StationName %in% HG] <- "HG"
@@ -443,12 +443,309 @@ counts.odv <- counts.aggregated_FISH
 counts.odv.sub <- counts.odv[,c("Depth", "Domain", "StationName", "conc.mn")]
 
 counts_data_for_ODV <- aggregate(conc.mn~StationName+Depth, counts.odv.sub,mean)
-env.for.odv <- metadata
+env.for.odv <- env
 
 
 counts_metadata_for_ODV <- env.for.odv %>% left_join(counts_data_for_ODV, by=c("StationName", "Depth"))
+counts_metadata_for_ODV$Layer[counts_metadata_for_ODV$Depth == "DCM"] <- "10"
+counts_metadata_for_ODV$Layer[counts_metadata_for_ODV$Depth == "EPI"] <- "100"
+counts_metadata_for_ODV$Layer[counts_metadata_for_ODV$Depth == "MESO"] <- "1000"
+counts_metadata_for_ODV$Layer[counts_metadata_for_ODV$Depth == "BATHY"] <- "2500"
+counts_metadata_for_ODV$Layer[counts_metadata_for_ODV$Depth == "ABYSS"] <- "5500"
+counts_metadata_for_ODV$conc.mn <- log10(counts_metadata_for_ODV$conc.mn)
 
-write.csv(counts_metadata_for_ODV, "~/CARD-FISH/CARD-FISH_water_project/counts_metadata_for_ODV.csv")
-counts_metadata_Depth_standard_for_ODV <- read.csv("~/CARD-FISH/CARD-FISH_water_project/Concentration/concentration_calculation/counts_metadata_depthstandarized_for_ODV.csv", sep = ",", dec = ".", header = TRUE)
-counts_metadata_Depth_standard_for_ODV$conc.mn <- log10(counts_metadata_Depth_standard_for_ODV$conc.mn)
-write.csv(counts_metadata_Depth_standard_for_ODV, "~/CARD-FISH/CARD-FISH_water_project/Concentration/concentration_calculation/counts_metadata_depthstandarized_for_ODV.csv")
+counts_metadata_for_ODV <- counts_metadata_for_ODV[,c("StationID", "StationName", "sample_ID", "Latitude..degrees_north.", "Longitude..degrees_east.", "Depth", "Layer", "Depth.m.", "Bot_Depth..m.", "Temperature", "Salinity", "Chla_fluor", "NO3", "NO3.NO2", "NH4", "NO2", "conc.mn")]
+
+write.csv(counts_metadata_for_ODV, "~/CARD-FISH/CARD-FISH_water_project/Concentration/counts_metadata_for_ODV.csv")
+
+#############################################################
+# Analyses with relative abundances (as a proportion of DAPI)
+#############################################################
+
+## Get proportions as a % of DAPI
+
+dapi.count.rel <- counts.aggregated_DAPI
+colnames(dapi.count.rel)[9] <- 'dapi.mn'
+colnames(dapi.count.rel)[11] <- 'dapi.sd' 
+
+fish.counts.rel <- counts.aggregated_FISH
+colnames(fish.counts.rel)[9] <- 'fish.mn'
+colnames(fish.counts.rel)[11] <- 'fish.sd' 
+
+counts.rel.m <- dapi.count.rel %>% left_join(fish.counts.rel, by=c("StationName", "Domain", "Depth"))
+
+counts.rel.m <- counts.rel.m[,c("StationName", "Depth", "Domain", "dapi.mn", "fish.mn")]
+
+counts.rel.m$proportion <- counts.rel.m$fish.mn*100/counts.rel.m$dapi.mn
+
+## PCA DCM  for Fish counts
+
+counts.rel.pca <- subset(counts.rel.m[counts.rel.m$Depth %in% c("DCM"),])
+counts.rel.pca <- counts.rel.pca[,c("Depth", "Domain", "StationName", "proportion")]
+counts.rel.pca %>% spread(Domain, proportion) -> counts.rel.pca.agg
+
+pca.rel <- counts.rel.pca.agg
+rownames(pca.rel) <- pca.rel$StationName
+
+pca.rel <- pca.rel[,c("ALT", "ARCH", "BACT", "CFX", "CREN", "DELTA", "EUB", "GAM", "OPI", "POL", "ROS", "SAR11", "SAR202", "SAR324", "SAR406", "VER")]
+
+pca.re.data.prcomp <- prcomp(pca.rel, center = TRUE,scale. = TRUE)
+summary(pca.re.data.prcomp)
+str(pca.re.data.prcomp)
+library(ggbiplot)
+
+ggbiplot(pca.re.data.prcomp, labels=rownames(pca.rel))
+
+pca.data.rel.Region <- c(rep("EGC", 2), rep("HG", 6), rep("N", 3), rep("SV", 1))
+
+ggbiplot(pca.re.data.prcomp,ellipse=TRUE,  labels=rownames(pca.rel), groups=pca.data.rel.Region)+theme_plot
+
+## RDA surface for Fish counts
+
+counts.rel.ra <- subset(counts.rel.m[counts.rel.m$Depth %in% c("DCM", "EPI"),])
+counts.rel.ra <- counts.rel.ra[,c("Depth", "Domain", "StationName", "proportion")]
+counts.rel.ra %>% spread(Domain, proportion) -> counts.rel.ra.agg
+counts.rel.ra.agg <- subset(counts.rel.ra.agg, StationName!="HG1")
+
+env.surf.DCM.EPI.only <- subset(env.surf[env.surf$Depth %in% c("DCM", "EPI"),])
+
+counts.rel.surf.metadata <- env.surf.DCM.EPI.only %>% left_join(counts.rel.ra.agg, by=c("StationName", "Depth"))
+counts.rel.surf.metadata$Depth.x <- NULL
+
+counts.rel.rda.stats <- counts.rel.surf.metadata[,c("ALT", "ARCH", "BACT", "CFX", "CREN", "DELTA", "EUB", "GAM", "OPI", "POL", "ROS", "SAR11", "SAR202", "SAR324", "SAR406", "VER")]
+rownames(counts.rel.rda.stats) <- counts.rel.surf.metadata$sample_ID
+rownames(env.surf.DCM.EPI.only) <- env.surf.DCM.EPI.only$sample_ID
+env.surf.DCM.EPI.only$sample_ID <- as.factor(env.surf.DCM.EPI.only$sample_ID)
+
+ord <- rda(counts.rel.rda.stats ~ Temperature + Chla_fluor + SiO3 + NO3, data=env.surf.DCM.EPI.only, scale = TRUE, center = FALSE) # scale FALSE since it is already scaled
+
+#quartz()
+plot(ord)
+
+#generate dataframe for the samples
+ps_scores <- vegan::scores(ord)
+sites <- data.frame(ps_scores$sites)
+sites$sample_ID <- as.factor(rownames(sites))
+
+sites <- sites %>%
+  left_join(env.surf.DCM.EPI.only)
+
+############arrows - no need to touch, just run it########
+# Now add the environmental variables as arrows
+arrowmatFL <- vegan::scores(ord, display = "bp")
+# Add labels, make a data.frame
+arrowdfFL <- data.frame(labels = rownames(arrowmatFL), arrowmatFL)
+# Define the arrow aesthetic mapping
+arrow_mapFL <- aes(xend = 1 *RDA1, 
+                   yend =1 * RDA2, 
+                   x = 0, 
+                   y = 0, 
+                   shape = NULL, 
+                   color = NULL, 
+                   label = labels)
+label_mapFL <- aes(x = 1.1 * RDA1, 
+                   y = 1.1 * RDA2, 
+                   shape = NULL, 
+                   color = NULL, 
+                   label = labels)
+arrowhead = arrow(length = unit(0.02, "npc"))
+#######
+
+#plot
+#evals_prop <- 100 * ord$CCA$eig[1:2] # NOT CORRECT VALUE!!!
+#use summary(ord) to add manually in the axis the "proportion explained" value
+#rda_plot <- ggplot() +
+#  geom_point(data = sites, aes(x = RDA1, y = RDA2, shape = Depth),  size = 4) +
+#  geom_text(data = sites,aes(x = RDA1, y = RDA2,label = StationName), nudge_y= 0.1,size=3)+
+#  guides(col = guide_legend(override.aes = list(size = 3))) +
+#  labs(x = sprintf("\nAxis1 [%s%% variance]", round(evals_prop[1], 2)), 
+#       y = sprintf("Axis2 [%s%% variance]\n", round(evals_prop[2], 2))) +
+#  coord_fixed(sqrt(ord$CCA$eig[2] / ord$CCA$eig[1])) +
+#  theme_plot+ 
+#  geom_segment(mapping = arrow_mapFL, size = .5, data = arrowdfFL, color = "black", arrow = arrowhead) + 
+#  geom_text(mapping = label_mapFL, size = 5, data = arrowdfFL, show.legend = FALSE, nudge_y= 0.1)+
+#  theme(panel.grid.minor = element_line(linetype = 'dotted', color = 'white', size= 0.5))+
+#  theme(panel.grid.major = element_line(linetype = 'dotted', color = 'white', size= 0.5))
+
+## NMDS DCM for FISH counts
+
+data.mnds <- pca.rel
+#set.seed(201)
+counts.mds <- metaMDS(data.mnds, maxit= 100, trace=TRUE)
+str(counts.mds) # gives the stress
+plot(counts.mds, type = "t")
+
+data.scores <- as.data.frame(scores(counts.mds))  
+data.scores$site <- rownames(data.scores) 
+data.scores$Region[data.scores$site %in% EGC] <- "EGC"
+data.scores$Region[data.scores$site %in% WSC] <- "WSC"
+species.scores <- as.data.frame(scores(counts.mds, "species")) 
+species.scores$species <- rownames(species.scores)
+
+
+nmds_rel_ab <- ggplot()+
+  #geom_polygon(data=hull.data,aes(x=NMDS1,y=NMDS2,fill=grp,group=grp),alpha=0.30) # check if this is possible
+  geom_text(data=species.scores,aes(x=NMDS1,y=NMDS2,label=species),alpha=0.5) + 
+  geom_point(data=data.scores,aes(x=NMDS1,y=NMDS2,shape=Region,colour=Region),size=2) + 
+  geom_text(data=data.scores,aes(x=NMDS1,y=NMDS2,label=site),size=4,vjust=-0.25)+
+  #stat_ellipse(data=data.scores,aes(x=NMDS1,y=NMDS2, fill=Region), alpha=.2,type='t',size =1, geom="polygon")+ #too few points to calculate an ellipse
+  scale_colour_manual(values=c("EGC" = "blue", "WSC" = "red")) +
+  coord_equal() +
+  theme_bw()+
+    theme(panel.background = element_blank(), 
+        panel.grid.major = element_blank(),  #remove major-grid labels
+        panel.grid.minor = element_blank(),  #remove minor-grid label
+        plot.background = element_blank())+
+  annotate("text", x =-0.3, y = -0.25, label=paste('Stress =',round(counts.mds$stress,3)))
+
+## ANOSIM water column with FISH counts
+
+counts.anosim <- counts.rel.m[,c("Depth", "Domain", "StationName", "proportion")]
+counts.anosim %>% spread(Domain, proportion) -> counts.anosim.sp
+
+counts.anosim.with.meta <- env %>% left_join(counts.anosim.sp, by=c("StationName", "Depth"))
+counts.anosim.with.meta$Region[counts.anosim.with.meta$StationName %in% EGC] <- "EGC"
+counts.anosim.with.meta$Region[counts.anosim.with.meta$StationName %in% WSC] <- "WSC"
+
+counts.rel.abs.dist <- dist(counts.anosim.with.meta[,c("ALT", "ARCH", "BACT", "CFX", "CREN", "DELTA", "EUB", "GAM", "OPI", "POL", "ROS", "SAR11", "SAR202", "SAR324", "SAR406", "VER")],"euclidean")
+
+adonis(counts.rel.abs.dist ~  Water_mass + Region + Depth, counts.anosim.with.meta)
+
+# Call:
+#   adonis(formula = counts.rel.abs.dist ~ Water_mass + Region +      Depth, data = counts.anosim.with.meta) 
+# 
+# Permutation: free
+# Number of permutations: 999
+# 
+# Terms added sequentially (first to last)
+# 
+#    Df   SumsOfSqs MeanSqs       F.Model   R2    Pr(>F)    
+# Water_mass  2   11707.4  5853.7 23.0479 0.49390  0.001 ***
+# Region      1     179.6   179.6  0.7072 0.00758  0.524    
+# Depth       3    1911.6   637.2  2.5088 0.08064  0.023 *  
+# Residuals  39    9905.2   254.0         0.41787           
+# Total      45   23703.8                 1.00000           
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# > 
+
+## ANOSIM water column with DAPI counts
+
+counts.anosim.d <- counts.rel.m[,c("Depth", "Domain", "StationName", "dapi.mn")]
+counts.anosim.d %>% spread(Domain, dapi.mn) -> counts.anosim.d.sp
+
+counts.anosim.d.with.meta <- env %>% left_join(counts.anosim.d.sp, by=c("StationName", "Depth"))
+counts.anosim.d.with.meta$Region[counts.anosim.d.with.meta$StationName %in% EGC] <- "EGC"
+counts.anosim.d.with.meta$Region[counts.anosim.d.with.meta$StationName %in% WSC] <- "WSC"
+
+counts.rel.abs.d.dist <- dist(counts.anosim.d.with.meta[,c("ALT", "ARCH", "BACT", "CFX", "CREN", "DELTA", "EUB", "GAM", "OPI", "POL", "ROS", "SAR11", "SAR202", "SAR324", "SAR406", "VER")],"euclidean")
+
+adonis(counts.rel.abs.d.dist ~  Water_mass + Region + Depth, counts.anosim.d.with.meta)
+
+# Call:
+#   adonis(formula = counts.rel.abs.d.dist ~ Water_mass + Region +      Depth, data = counts.anosim.d.with.meta) 
+# 
+# Permutation: free
+# Number of permutations: 999
+# 
+# Terms added sequentially (first to last)
+# 
+#      Df      SumsOfSqs    MeanSqs    F.Model  R2     Pr(>F)    
+# Water_mass  2 1.6520e+14 8.2598e+13  55.833 0.44167  0.001 ***
+# Region      1 1.9987e+13 1.9987e+13  13.511 0.05344  0.003 ** 
+# Depth       3 1.3115e+14 4.3715e+13  29.550 0.35064  0.001 ***
+# Residuals  39 5.7695e+13 1.4794e+12         0.15426           
+# Total      45 3.7402e+14                    1.00000           
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+## TEST!!! Permanova for water column
+
+# Normality test proportions in DCM: p-values should be greater than the significance level 0.05, some are normality distributed, some not
+
+shapiro.test(counts.anosim.with.meta$ALT) # p-value = 7.149e-07 
+shapiro.test(counts.anosim.with.meta$ARCH) # p-value = 0.01214
+shapiro.test(counts.anosim.with.meta$BACT) # p-value = 1.185e-07
+shapiro.test(counts.anosim.with.meta$CFX) # p-value = 0.01663
+shapiro.test(counts.anosim.with.meta$CREN) # p-value = 1.272e-07
+shapiro.test(counts.anosim.with.meta$DELTA) # p-value = 0.02678
+shapiro.test(counts.anosim.with.meta$EUB) # p-value = 0.3208    <-
+shapiro.test(counts.anosim.with.meta$GAM) # p-value = 1.81e-07
+shapiro.test(counts.anosim.with.meta$OPI) # p-value = 2.495e-05
+shapiro.test(counts.anosim.with.meta$POL) # p-value = 8.699e-09
+shapiro.test(counts.anosim.with.meta$ROS) # p-value = 4.434e-06
+shapiro.test(counts.anosim.with.meta$SAR11) # p-value = 0.005482
+shapiro.test(counts.anosim.with.meta$SAR202) # p-value = 2.255e-05
+shapiro.test(counts.anosim.with.meta$SAR324) # p-value = 0.0001698
+shapiro.test(counts.anosim.with.meta$SAR406) # p-value = 7.316e-08
+shapiro.test(counts.anosim.with.meta$VER) # p-value = 6.419e-07
+
+# We assume anyway that the data is normally distributed since we trasnformed to proportions?
+
+## perMANOVA 
+
+adonis2(counts.rel.abs.dist ~ Water_mass + Depth + Region, counts.anosim.with.meta)
+
+# Permutation test for adonis under reduced model
+# Terms added sequentially (first to last)
+# Permutation: free
+# Number of permutations: 999
+# 
+# adonis2(formula = counts.rel.abs.dist ~ Water_mass + Depth + Region, data = counts.anosim.with.meta)
+# Df SumOfSqs      R2       F Pr(>F)    
+# Water_mass  2  11707.4 0.49390 23.0479  0.001 ***
+#   Depth       3   1873.1 0.07902  2.4584  0.018 *  
+#   Region      1    218.1 0.00920  0.8586  0.439    
+# Residual   39   9905.2 0.41787                   
+# Total      45  23703.8 1.00000                   
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+## perMANOVA for EUB at DCM 
+
+counts.anosim.eub <- subset(counts.rel.m[counts.rel.m$Domain %in% c("EUB"),])
+counts.anosim.eub %>% spread(Domain, proportion) -> counts.anosim.eub
+
+
+eub.anosim.with.meta <- env %>% left_join(counts.anosim.eub, by=c("StationName", "Depth"))
+eub.anosim.with.meta$Region[eub.anosim.with.meta$StationName %in% EGC] <- "EGC"
+eub.anosim.with.meta$Region[eub.anosim.with.meta$StationName %in% WSC] <- "WSC"
+eub.anosim.with.meta <- subset(eub.anosim.with.meta[eub.anosim.with.meta$Depth %in% c("DCM"),])
+
+counts.eub.rel.abs.dist <- dist(eub.anosim.with.meta[,c("EUB")],"euclidean")
+
+adonis2(counts.eub.rel.abs.dist ~  Water_mass + Region, eub.anosim.with.meta)
+
+# Permutation test for adonis under reduced model
+# Terms added sequentially (first to last)
+# Permutation: free
+# Number of permutations: 999
+# 
+# adonis2(formula = counts.eub.rel.abs.dist ~ Water_mass + Region, data = eub.anosim.with.meta)
+# Df SumOfSqs      R2      F Pr(>F)
+# Water_mass  1   208.59 0.15882 1.7084  0.227
+# Region      1     5.92 0.00450 0.0485  0.825
+# Residual    9  1098.88 0.83668              
+# Total      11  1313.38 1.00000   
+
+## perMANOVA for DAPI: note shapiro test shown not normal distribution
+
+adonis2(counts.rel.abs.d.dist ~  Water_mass + Region + Depth, counts.anosim.d.with.meta)
+
+# Permutation test for adonis under reduced model
+# Terms added sequentially (first to last)
+# Permutation: free
+# Number of permutations: 999
+# 
+# adonis2(formula = counts.rel.abs.d.dist ~ Water_mass + Region + Depth, data = counts.anosim.d.with.meta)
+# Df   SumOfSqs      R2      F Pr(>F)    
+# Water_mass  2 1.6520e+14 0.44167 55.833  0.001 ***
+#   Region      1 1.9987e+13 0.05344 13.511  0.002 ** 
+#   Depth       3 1.3115e+14 0.35064 29.550  0.001 ***
+#   Residual   39 5.7695e+13 0.15426                  
+# Total      45 3.7402e+14 1.00000                  
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+
+

10439e4f0e6999621554de2cb25052d2d04d6635
edfadeev <efadeev@awi.de>
2019-06-24 17:19:12 +0200

    Update Data_import_and_pre-processing(1).R
    Changes:
    1. Station SV2 is removed from the metadata before the scaling.
    2. Scaling streamlined using dplyr.
    3. Optimized concentration calculations- first conc. per field of view was calculated and then mean per sample.
    4. Longitude coordinates are fed directly from the metadata in a for loop
    
    All old rows of the script are currently commented, should be removed in later stages.

--- Rwork/Data_import_and_pre-processing(1).R
+++ Rwork/Data_import_and_pre-processing(1).R
@@ -4,7 +4,9 @@
 
 setwd("~/CARD-FISH/CARD-FISH_water_project/")
 
-#load("card_fish_water_column_ps99.Rdata")
+#set working directory in windows
+#wd <- dirname(rstudioapi::getActiveDocumentContext()$path)
+#setwd(wd)
 ###################################
 ## Load required libraries
 ###################################
@@ -17,66 +19,113 @@ library(splines)
 library(gridExtra)
 
 ###################################
-## import data set
+## import environmental data and scale to center
+###################################
+#import data
+metadata <- read.csv("PS99_samples_meta_EF_MC_nutrient_corrected.csv", sep = ",", dec = ".", header = TRUE)
+
+#remove station SV2 from the dataset
+metadata <- subset(metadata, !StationName == "SV2")
+
+#list environmental parameters that need to be scaled
+env.par= c("Temperature","Salinity", 
+           "Chla_fluor", "NO3.NO2", 
+           "NO2", "NO3", 
+           "SiO3", "PO4",
+           "NH4")
+
+#scale parameters
+scale1 <- function(x) scale(x, center = FALSE, scale = TRUE)[,1]
+
+metadata %>% 
+  mutate_at(env.par, scale1) -> env
+
+# #Data standarization
+# env_raw <- metadata
+# 
+# centre_scale2 <- function(x) {
+#   scale(x, scale = FALSE)
+# }
+# env.par= c("Temperature","Salinity", "Chla_fluor", "NO3.NO2", "NO2", "NO3", "SiO3", "PO4","NH4")
+# env=env_raw
+# env[,env.par]<- apply(env[,env.par], MARGIN=2, FUN=centre_scale2)
+# env <- subset(env, !StationName == "SV2")
+
+###################################
+## Import counts and calculate cell concentrations
 ###################################
 #raw counts
 raw.counts.SH <- read.csv("FOV_all_groups_SH.csv", sep = ",", dec = ".", header = TRUE)
 
-#environmental data
-metadata <- read.csv("PS99_samples_meta_EF_MC_nutrient_corrected.csv", sep = ",", dec = ".", header = TRUE)
-
-#Data standarization
-env_raw <- metadata
-
-centre_scale2 <- function(x) {
-  scale(x, scale = FALSE)
-}
-
-env.par= c("Temperature","Salinity", "Chla_fluor", "NO3.NO2", "NO2", "NO3", "SiO3", "PO4","NH4")
-env=env_raw
-env[,env.par]<- apply(env[,env.par], MARGIN=2, FUN=centre_scale2)
-env <- subset(env, !StationName == "SV2")
-
-###################################
-## calculate cell concentration
-###################################
-#calculation factor
+#microscope calculation factor
 calc.factor <- 99515.5458411807
 
-#cell concentration for DAPI
-counts_DAPI <- as.data.frame(as.list((aggregate(DAPI_Nr_Set ~SAMPLE_NAME, data=raw.counts.SH, 
-                                                           FUN = function(x) c(mn = mean(x), md =  median(x), sd = sd(x), n = length(x))))))
-counts_DAPI.vol <- as.data.frame(as.list((aggregate(Volume ~SAMPLE_NAME, data=raw.counts.SH, 
-                                                                  FUN = mean))))
+###DAPI
+#calculate cell concentration for each field of view
+raw.counts.SH$DAPI_conc <- (raw.counts.SH$DAPI_Nr_Set*calc.factor)/raw.counts.SH$Volume
 
-counts_DAPI <- cbind(counts_DAPI,counts_DAPI.vol[,2])
-colnames(counts_DAPI)[6] <- "volume"
-
-counts_DAPI$conc.mn <- (counts_DAPI$DAPI_Nr_Set.mn*calc.factor)/counts_DAPI$volume
-counts_DAPI$conc.md <- (counts_DAPI$DAPI_Nr_Set.md*calc.factor)/counts_DAPI$volume
-counts_DAPI$conc.sd <- (counts_DAPI$DAPI_Nr_Set.sd*calc.factor)/counts_DAPI$volume
-
+counts_DAPI <- as.data.frame(as.list((aggregate(DAPI_conc~SAMPLE_NAME, 
+                                                data=raw.counts.SH, 
+                                                FUN = function(x) c(conc.mn = mean(x), conc.md =  median(x), conc.sd = sd(x), n = length(x))))))
+#split sample name
 counts_DAPI <- counts_DAPI %>% 
   separate(SAMPLE_NAME, c("StationName", "Depth", "Domain"),"-")
 
+#remove SV2 station
 counts_DAPI <- subset(counts_DAPI, !StationName == "SV2")
 
+# 
+# counts_DAPI <- as.data.frame(as.list((aggregate(DAPI_Nr_Set~SAMPLE_NAME, 
+#                                                 data=raw.counts.SH, 
+#                                                 FUN = function(x) c(mn = mean(x), md =  median(x), sd = sd(x), n = length(x))))))
+# 
+# counts_DAPI.vol <- as.data.frame(as.list((aggregate(Volume~SAMPLE_NAME, 
+#                                                     data=raw.counts.SH, 
+#                                                     FUN = mean))))
+# 
+# counts_DAPI <- cbind(counts_DAPI,counts_DAPI.vol[,2])
+# colnames(counts_DAPI)[6] <- "volume"
+# 
+# counts_DAPI$conc.mn <- (counts_DAPI$DAPI_Nr_Set.mn*calc.factor)/counts_DAPI$volume
+# counts_DAPI$conc.md <- (counts_DAPI$DAPI_Nr_Set.md*calc.factor)/counts_DAPI$volume
+# counts_DAPI$conc.sd <- (counts_DAPI$DAPI_Nr_Set.sd*calc.factor)/counts_DAPI$volume
+# 
+# counts_DAPI <- counts_DAPI %>% 
+#   separate(SAMPLE_NAME, c("StationName", "Depth", "Domain"),"-")
+# 
+# counts_DAPI <- subset(counts_DAPI, !StationName == "SV2")
+
+
+###FISH
 #cell concentration for FISH
-counts_FISH <- as.data.frame(as.list(aggregate(DAPI_Nr_SubSet ~SAMPLE_NAME, data=raw.counts.SH, 
-                                                          FUN = function(x) c(mn = mean(x), md =  median(x), sd = sd(x), n = length(x)))))
-counts_FISH.vol <- as.data.frame(as.list((aggregate(Volume ~SAMPLE_NAME, data=raw.counts.SH, 
-                                                                  FUN = mean))))
+raw.counts.SH$FISH_conc <- (raw.counts.SH$DAPI_Nr_SubSet*calc.factor)/raw.counts.SH$Volume
 
-counts_FISH <- cbind(counts_FISH,counts_FISH.vol[,2])
-colnames(counts_FISH)[6] <- "volume"
-
-counts_FISH$conc.mn <- (counts_FISH$DAPI_Nr_SubSet.mn*calc.factor)/counts_FISH$volume
-counts_FISH$conc.md <- (counts_FISH$DAPI_Nr_SubSet.md*calc.factor)/counts_FISH$volume
-counts_FISH$conc.sd <- (counts_FISH$DAPI_Nr_SubSet.sd*calc.factor)/counts_FISH$volume
-
+counts_FISH <- as.data.frame(as.list((aggregate(FISH_conc~SAMPLE_NAME, 
+                                                data=raw.counts.SH, 
+                                                FUN = function(x) c(conc.mn = mean(x), conc.md =  median(x), conc.sd = sd(x), n = length(x))))))
+#split sample name
 counts_FISH <- counts_FISH %>% 
   separate(SAMPLE_NAME, c("StationName", "Depth", "Domain"),"-")
+
+#remove SV2 station
 counts_FISH <- subset(counts_FISH, !StationName == "SV2")
+
+
+# counts_FISH <- as.data.frame(as.list(aggregate(DAPI_Nr_SubSet ~SAMPLE_NAME, data=raw.counts.SH,
+#                                                           FUN = function(x) c(mn = mean(x), md =  median(x), sd = sd(x), n = length(x)))))
+# counts_FISH.vol <- as.data.frame(as.list((aggregate(Volume ~SAMPLE_NAME, data=raw.counts.SH,
+#                                                                   FUN = mean))))
+# 
+# counts_FISH <- cbind(counts_FISH,counts_FISH.vol[,2])
+# colnames(counts_FISH)[6] <- "volume"
+# 
+# counts_FISH$conc.mn <- (counts_FISH$DAPI_Nr_SubSet.mn*calc.factor)/counts_FISH$volume
+# counts_FISH$conc.md <- (counts_FISH$DAPI_Nr_SubSet.md*calc.factor)/counts_FISH$volume
+# counts_FISH$conc.sd <- (counts_FISH$DAPI_Nr_SubSet.sd*calc.factor)/counts_FISH$volume
+# 
+# counts_FISH <- counts_FISH %>%
+#   separate(SAMPLE_NAME, c("StationName", "Depth", "Domain"),"-")
+# counts_FISH <- subset(counts_FISH, !StationName == "SV2")
 
 ##################################
 ## call water layers and add regions and water masses
@@ -95,33 +144,41 @@ counts_DAPI$Region[counts_DAPI$StationName %in% WSC] <- "WSC"
 counts_FISH$Region[counts_FISH$StationName %in% EGC] <- "EGC"
 counts_FISH$Region[counts_FISH$StationName %in% WSC] <- "WSC" 
 
-#add the longitude
-counts_FISH$long[counts_FISH$StationName == "EG1"] <- -5.418
-counts_FISH$long[counts_FISH$StationName == "EG4"] <- -2.729
-counts_FISH$long[counts_FISH$StationName == "HG1"] <- 6.088
-counts_FISH$long[counts_FISH$StationName == "HG2"] <- 4.907
-counts_FISH$long[counts_FISH$StationName == "HG4"] <- 4.185
-counts_FISH$long[counts_FISH$StationName == "HG5"] <- 3.8
-counts_FISH$long[counts_FISH$StationName == "HG7"] <- 3.5
-counts_FISH$long[counts_FISH$StationName == "HG9"] <- 2.841
-counts_FISH$long[counts_FISH$StationName == "N4"] <- 4.508
-counts_FISH$long[counts_FISH$StationName == "N3"] <- 5.166
-counts_FISH$long[counts_FISH$StationName == "N5"] <- 3.062
-#counts_FISH$long[counts_FISH$StationName == "SV2"] <- 9.514
+#add the longitude of each station from the metadata
+for (station in metadata$StationName){
+  counts_DAPI$long[counts_DAPI$StationName == station] <- metadata[metadata$StationName == station,]$Longitude..degrees_east.
+  counts_FISH$long[counts_FISH$StationName == station] <- metadata[metadata$StationName == station,]$Longitude..degrees_east.
+  }
 
-counts_DAPI$long[counts_DAPI$StationName == "EG1"] <- -5.418
-counts_DAPI$long[counts_DAPI$StationName == "EG4"] <- -2.729
-counts_DAPI$long[counts_DAPI$StationName == "HG1"] <- 6.088
-counts_DAPI$long[counts_DAPI$StationName == "HG2"] <- 4.907
-counts_DAPI$long[counts_DAPI$StationName == "HG4"] <- 4.185
-counts_DAPI$long[counts_DAPI$StationName == "HG5"] <- 3.8
-counts_DAPI$long[counts_DAPI$StationName == "HG7"] <- 3.5
-counts_DAPI$long[counts_DAPI$StationName == "HG9"] <- 2.841
-counts_DAPI$long[counts_DAPI$StationName == "N4"] <- 4.508
-counts_DAPI$long[counts_DAPI$StationName == "N3"] <- 5.166
-counts_DAPI$long[counts_DAPI$StationName == "N5"] <- 3.062
-#counts_DAPI$long[counts_DAPI$StationName == "SV2"] <- 9.514
+# counts_FISH$long[counts_FISH$StationName == "EG1"] <- -5.418
+# counts_FISH$long[counts_FISH$StationName == "EG4"] <- -2.729
+# counts_FISH$long[counts_FISH$StationName == "HG1"] <- 6.088
+# counts_FISH$long[counts_FISH$StationName == "HG2"] <- 4.907
+# counts_FISH$long[counts_FISH$StationName == "HG4"] <- 4.185
+# counts_FISH$long[counts_FISH$StationName == "HG5"] <- 3.8
+# counts_FISH$long[counts_FISH$StationName == "HG7"] <- 3.5
+# counts_FISH$long[counts_FISH$StationName == "HG9"] <- 2.841
+# counts_FISH$long[counts_FISH$StationName == "N4"] <- 4.508
+# counts_FISH$long[counts_FISH$StationName == "N3"] <- 5.166
+# counts_FISH$long[counts_FISH$StationName == "N5"] <- 3.062
+# #counts_FISH$long[counts_FISH$StationName == "SV2"] <- 9.514
+# 
+# counts_DAPI$long[counts_DAPI$StationName == "EG1"] <- -5.418
+# counts_DAPI$long[counts_DAPI$StationName == "EG4"] <- -2.729
+# counts_DAPI$long[counts_DAPI$StationName == "HG1"] <- 6.088
+# counts_DAPI$long[counts_DAPI$StationName == "HG2"] <- 4.907
+# counts_DAPI$long[counts_DAPI$StationName == "HG4"] <- 4.185
+# counts_DAPI$long[counts_DAPI$StationName == "HG5"] <- 3.8
+# counts_DAPI$long[counts_DAPI$StationName == "HG7"] <- 3.5
+# counts_DAPI$long[counts_DAPI$StationName == "HG9"] <- 2.841
+# counts_DAPI$long[counts_DAPI$StationName == "N4"] <- 4.508
+# counts_DAPI$long[counts_DAPI$StationName == "N3"] <- 5.166
+# counts_DAPI$long[counts_DAPI$StationName == "N5"] <- 3.062
+# #counts_DAPI$long[counts_DAPI$StationName == "SV2"] <- 9.514
+# 
 
-
+##################################
+#save image for further analysis
+##################################
 save.image("card_fish_water_column_ps99.Rdata")
 
